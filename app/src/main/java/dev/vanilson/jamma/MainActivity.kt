158f127047b6b74dea2c0c7e08566f556c2bd01a
Vanilson Fernandes <vanilson.fernandes@calindra.com.br>
2024-08-25 17:37:03 +0100

    feat: app icon

--- app/src/main/java/dev/vanilson/jamma/MainActivity.kt
+++ app/src/main/java/dev/vanilson/jamma/MainActivity.kt
@@ -13,14 +13,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -52,9 +56,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     floatingActionButton = {
                         FloatingActionButton(
-                            onClick = { onFabClick() }
+                            onClick = { onFabClick() },
+                            containerColor = Color(14, 27, 37),
                         ) {
-                            Text(text = "+")
+                            Icon(Icons.Default.Add, contentDescription = "Add")
                         }
                     }
                 ) { innerPadding ->
