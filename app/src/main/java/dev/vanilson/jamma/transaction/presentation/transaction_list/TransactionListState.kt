3eb767d058e273b9bd91b491cd189da667c2412e
Vanilson Fernandes <vanilson.fernandes@gmail.com>
2025-09-12 08:45:51 +0100

    fix: Update totalBalance format in TransactionListState to use comma as decimal separator

--- app/src/main/java/dev/vanilson/jamma/transaction/presentation/transaction_list/TransactionListState.kt
+++ app/src/main/java/dev/vanilson/jamma/transaction/presentation/transaction_list/TransactionListState.kt
@@ -11,7 +11,7 @@ data class TransactionListState(
     val dayAmount: String = "$ 99.99",
     val weekAmount: String = "$ 999.99",
     val monthAmount: String = "$ 9 999.99",
-    val totalBalance: String = "$ 999 999.99",
+    val totalBalance: String = "$ 0,00",
     val isAddingTransaction: Boolean = false,
     val transactionToDelete: TransactionUI? = null,
 )
