4afda64a0c6da85067c205ecf8920bad38dd82ae
Vanilson Fernandes <vanilson.fernandes@gmail.com>
2025-09-20 22:16:28 +0100

    feat: Enhance SaveTransactionAndUpdateBalance use case; add logic to adjust balance on transaction amount changes

--- app/src/main/java/dev/vanilson/jamma/transaction/domain/usecase/SaveTransactionAndUpdateBalanceUseCase.kt
+++ app/src/main/java/dev/vanilson/jamma/transaction/domain/usecase/SaveTransactionAndUpdateBalanceUseCase.kt
@@ -26,10 +26,23 @@ class SaveTransactionAndUpdateBalanceUseCase(
         if (transactionUI.uid != 0) {
             Timber.i(">>> Updating existing transaction with ID: ${transactionUI.uid}")
             transactionRepository.findById(transactionUI.uid).let {
+                if (it.amountInCents != transactionUI.amount.amountInCents) {
+                    Timber.i(">>> Transaction amount has changed from ${it.amountInCents} to ${transactionUI.amount.amountInCents}")
+                    //amount changed, need to adjust balance
+                    if (transactionUI.income) {
+                        //was income, still is income, just update the difference
+                        amountToAdd = transactionUI.amount.amountInCents - it.amountInCents
+                    } else {
+                        //was expense, still is expense, just update the difference
+                        amountToDeduct = transactionUI.amount.amountInCents - it.amountInCents
+                    }
+                    Timber.i(">>> Adjusting balance due to amount change. Deducting: $amountToDeduct, Adding: $amountToAdd")
+                }
+
                 val wasPaid = it.paidDateTime != null
                 if (wasPaid && transactionUI.isPaid.not()) {
                     //need to charge the balance
-                    amountToAdd = transactionUI.amount.amountInCents
+                    amountToAdd = amountToAdd + transactionUI.amount.amountInCents
                 } else if (wasPaid.not() && transactionUI.isPaid) {
                     //need to discharge the balance
                     amountToDeduct = transactionUI.amount.amountInCents
@@ -45,6 +58,9 @@ class SaveTransactionAndUpdateBalanceUseCase(
                 } else {
                     amountToDeduct = transactionUI.amount.amountInCents
                 }
+            } else {
+                Timber.i(">>> New transaction is not paid. No balance update needed.")
+                return
             }
         }
 
