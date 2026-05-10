8704dd57f4fda30f931a1fab672037e92e1b9458
Vanilson Fernandes <vanilson.fernandes@gmail.com>
2026-05-10 12:02:33 +0100

    chore: Bump version to 0.4.0; add ProGuard rule to suppress javax.annotation warnings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.vanilson.jamma"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.3.0"
+        versionCode = 6
+        versionName = "0.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
